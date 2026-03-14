58a3679e71256522f04db941b67d75fb9ddf5d2f
GaneshIjjurouthu-0852 <ganeshijjurouthu.1205@gmail.com>
2026-03-14 21:55:17 +0530

    Day-3 Buy_Sell_Stock + LongestSubstring

---
+++ easy/121_but_and_sell.py
@@ -0,0 +1,10 @@
+def maxProfit(prices):
+    min_price=float('inf')
+    max_profit=0
+    for price in prices:
+        if price < min_price:
+            min_price=min(price,min_price)
+        else:
+            max_profit=max(max_profit,price-min_price)
+    return max_profit
+print(maxProfit([7,1,5,3,6,4]))
